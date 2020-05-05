dd11ec87d38468f9856acbb1bebdacf4467d5542
Ismael <barracuda.framework@gmail.com>
2020-05-05 18:59:48 +0200

    try automatic publication in pypi

--- barracuda/__main__.py
+++ barracuda/__main__.py
@@ -2,17 +2,7 @@ import sys
 
 from PySide2.QtWidgets import QApplication
 
-from barracuda.main_window import MainWindow
+from barracuda.main_window import execute
 
 if __name__ == '__main__':
-    # You need one (and only one) QApplication instance per application.
-    # Pass in sys.argv to allow command line arguments for your app.
-    # If you know you won't use command line arguments
-    # QApplication([]) works too.
-    app = QApplication(sys.argv)
-
-    window = MainWindow()
-    window.show()  # IMPORTANT!!!!! Windows are hidden by default.
-
-    # Start the event loop.
-    app.exec_()
+    execute()
