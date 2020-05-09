fcacc33a7c8638c3a193d53b433f176a8c178bf1
Ismael <barracuda.framework@gmail.com>
2020-05-09 15:25:09 +0200

    reformat pip8 compliant

--- mamba_server/utils/component.py
+++ mamba_server/utils/component.py
@@ -1,6 +1,7 @@
 """Component handling utility functions"""
 
-from mamba_server.exceptions import ComponentSettingsException, ComponentConfigException
+from mamba_server.exceptions import ComponentSettingsException
+from mamba_server.exceptions import ComponentConfigException
 
 
 def generate_component_configuration(settings=None,
@@ -8,27 +9,30 @@ def generate_component_configuration(settings=None,
                                      local_config=None):
     """Returns a dictionary with the component configuration.
 
-    Local configurations passed via launch file have precedence over configuration file settings.
+    Local configurations passed via launch file have precedence over
+    configuration file settings.
 
-    The configuration dictionary is composed of the local configuration, then the configurations
-    from file that are not in local, and then the settings that are neither in file nor in local
-    but have default values.
+    The configuration dictionary is composed of the local configuration, then
+    the configurations from file that are not in local, and then the settings
+    that are neither in file nor in local but have default values.
 
     Args:
-        settings (dict, optional): The dictionary with the description of the component settings.
-        config_file (dict, optional): The dictionary of the component settings retrieved from
-                                      component.config.json.
-        local_config (dict, optional): The dictionary of the component settings passed via
-                                       launch file.
+        settings (dict, optional): The dictionary with the description of the
+                                   component settings.
+        config_file (dict, optional): The dictionary of the component settings
+                                      retrieved from component.config.json.
+        local_config (dict, optional): The dictionary of the component
+                                       settings passed via launch file.
 
     Returns:
         dict: The validated component configuration parameters.
 
     Raises:
-        ComponentConfigException: If a required setting is not present in local nor
-                                  in file parameters.
-        ComponentSettingsException: If a setting is missing the 'required' field or the
-                                    'default' field if it is not a required parameter.
+        ComponentConfigException: If a required setting is not present in
+                                  local nor in file parameters.
+        ComponentSettingsException: If a setting is missing the 'required'
+                                    field or the 'default' field if it is not
+                                    a required parameter.
 
     """
 
@@ -36,9 +40,11 @@ def generate_component_configuration(settings=None,
     config_file = config_file or {}
     local_config = local_config or {}
 
-    composed_config = dict(list(config_file.items()) + list(local_config.items()))
+    composed_config = dict(
+        list(config_file.items()) + list(local_config.items()))
 
-    component_name = composed_config['name'] if 'name' in composed_config else ''
+    component_name = composed_config[
+        'name'] if 'name' in composed_config else ''
 
     for key, value in settings.items():
         if key not in composed_config:
@@ -47,7 +53,9 @@ def generate_component_configuration(settings=None,
                     f'Setting "{key}" is missing required field "required"')
             if value['required']:
                 raise ComponentConfigException(
-                    f'Component "{component_name}" configuration is missing parameter "{key}"')
+                    f'Component "{component_name}" configuration is missing '
+                    f'parameter "{key}"'
+                )
             if (not value['required']) and ('default' not in value):
                 raise ComponentSettingsException(
                     f'Setting "{key}" is missing required field "default"')
