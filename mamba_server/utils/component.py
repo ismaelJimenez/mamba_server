a1f6f53d828bb2c3fb60ed07de9e695894b27393
Ismael <barracuda.framework@gmail.com>
2020-05-09 13:16:12 +0200

    reformat component utilities

--- mamba_server/utils/component.py
+++ mamba_server/utils/component.py
@@ -1,48 +1,58 @@
+"""Component handling utility functions"""
+
 from mamba_server.exceptions import ComponentSettingsException, ComponentConfigException
 
 
 def generate_component_configuration(component_name="",
-                                     settings={},
-                                     config_file={},
-                                     local_config={}):
+                                     settings=None,
+                                     config_file=None,
+                                     local_config=None):
     """Returns a dictionary with the component configuration.
 
     Local configurations passed via launch file have precedence over configuration file settings.
 
-    The configuration dictionary is composed of the local configuration, then the configurations from file
-    that are not in local, and then the settings that are neither in file nor in local but have default values.
+    The configuration dictionary is composed of the local configuration, then the configurations
+    from file that are not in local, and then the settings that are neither in file nor in local
+    but have default values.
 
     Args:
-        component_name (str, optional): The component name. Only used for feedback on the error handling.
+        component_name (str, optional): The component name. Only used for feedback on the error
+                                        handling.
         settings (dict, optional): The dictionary with the description of the component settings.
-        config_file (dict, optional): The dictionary of the component settings retrieved from component.config.json.
-        local_config (dict, optional): The dictionary of the component settings passed via launch file.
+        config_file (dict, optional): The dictionary of the component settings retrieved from
+                                      component.config.json.
+        local_config (dict, optional): The dictionary of the component settings passed via
+                                       launch file.
 
     Returns:
         dict: The validated component configuration parameters.
 
     Raises:
-        ComponentConfigException: If a required setting is not present in local nor in file parameters.
-        ComponentSettingsException: If a setting is missing the 'required' field or the 'default' field if it is not a required parameter.
+        ComponentConfigException: If a required setting is not present in local nor
+                                  in file parameters.
+        ComponentSettingsException: If a setting is missing the 'required' field or the
+                                    'default' field if it is not a required parameter.
 
     """
 
-    composed_config = dict(
-        list(config_file.items()) + list(local_config.items()))
+    settings = settings or {}
+    config_file = config_file or {}
+    local_config = local_config or {}
+
+    composed_config = dict(list(config_file.items()) + list(local_config.items()))
 
     for key, value in settings.items():
         if key not in composed_config:
             if 'required' not in value:
                 raise ComponentSettingsException(
                     f'Setting "{key}" is missing required field "required"')
-            elif value['required']:
+            if value['required']:
                 raise ComponentConfigException(
-                    f'Component "{component_name}" configuration is missing parameter "{key}"'
-                )
-            elif (not value['required']) and ('default' not in value):
+                    f'Component "{component_name}" configuration is missing parameter "{key}"')
+            if (not value['required']) and ('default' not in value):
                 raise ComponentSettingsException(
                     f'Setting "{key}" is missing required field "default"')
-            else:
-                composed_config[key] = value['default']
+
+            composed_config[key] = value['default']
 
     return composed_config
--- tests/unit_tests/test_utils/test_component.py
+++ tests/unit_tests/test_utils/test_component.py
@@ -9,25 +9,20 @@ def test_generate_component_configuration_wo_settings_file_local():
 
 
 def test_generate_component_configuration_composition_from_file_wo_local():
-    assert component.generate_component_configuration(
-        config_file={'param_1': 1}) == {
-            'param_1': 1
-        }
+    assert component.generate_component_configuration(config_file={'param_1': 1}) == {'param_1': 1}
 
 
 def test_generate_component_configuration_composition_from_local_wo_file():
-    assert component.generate_component_configuration(
-        local_config={'param_1': 1}) == {
-            'param_1': 1
-        }
+    assert component.generate_component_configuration(local_config={'param_1': 1}) == {
+        'param_1': 1
+    }
 
 
-def test_generate_component_configuration_composition_local_precedence_over_file(
-):
-    assert component.generate_component_configuration(
-        config_file={'param_1': 2}, local_config={'param_1': 1}) == {
-            'param_1': 1
-        }
+def test_generate_component_configuration_composition_local_precedence_over_file():
+    assert component.generate_component_configuration(config_file={'param_1': 2},
+                                                      local_config={'param_1': 1}) == {
+                                                          'param_1': 1
+                                                      }
     assert component.generate_component_configuration(config_file={
         'param_1': 2,
         'param_3': 3
@@ -42,30 +37,25 @@ def test_generate_component_configuration_composition_local_precedence_over_file
                                                       }
 
 
-def test_generate_component_configuration_settings_existing_required_parameter(
-):
+def test_generate_component_configuration_settings_existing_required_parameter():
     assert component.generate_component_configuration(
-        settings={
-            'param_1': {
-                'description': 'Param 1 description',
-                'required': True
-            }
-        },
+        settings={'param_1': {
+            'description': 'Param 1 description',
+            'required': True
+        }},
         config_file={'param_1': 1}) == {
             'param_1': 1
         }
 
 
-def test_generate_component_configuration_settings_non_existing_required_parameter(
-):
+def test_generate_component_configuration_settings_non_existing_required_parameter():
     with pytest.raises(ComponentConfigException) as excinfo:
-        component.generate_component_configuration(settings={
-            'param_2': {
+        component.generate_component_configuration(
+            settings={'param_2': {
                 'description': 'Param 2 description',
                 'required': True
-            }
-        },
-                                                   config_file={'param_1': 1})
+            }},
+            config_file={'param_1': 1})
 
     assert 'missing parameter' in str(excinfo.value)
 
@@ -75,12 +65,10 @@ def test_generate_component_configuration_settings_non_existing_required_paramet
     with pytest.raises(ComponentConfigException) as excinfo:
         component.generate_component_configuration(
             component_name='test_component_name',
-            settings={
-                'param_2': {
-                    'description': 'Param 2 description',
-                    'required': True
-                }
-            },
+            settings={'param_2': {
+                'description': 'Param 2 description',
+                'required': True
+            }},
             config_file={'param_1': 1})
 
     assert 'test_component_name' in str(excinfo.value)
@@ -98,33 +86,28 @@ def test_generate_component_configuration_settings_missing_required_field():
     assert 'missing required field "required"' in str(excinfo.value)
 
 
-def test_generate_component_configuration_settings_non_existing_optional_parameter_wo_default(
-):
+def test_generate_component_configuration_settings_non_existing_optional_parameter_wo_default():
     with pytest.raises(ComponentSettingsException) as excinfo:
         assert component.generate_component_configuration(
-            settings={
-                'param_2': {
-                    'description': 'Param 2 description',
-                    'required': False
-                }
-            },
+            settings={'param_2': {
+                'description': 'Param 2 description',
+                'required': False
+            }},
             config_file={'param_1': 1})
 
     assert 'param_2' in str(excinfo.value)
     assert 'missing required field "default"' in str(excinfo.value)
 
 
-def test_generate_component_configuration_settings_non_existing_optional_parameter_w_default(
-):
-    assert component.generate_component_configuration(
-        settings={
-            'param_2': {
-                'description': 'Param 2 description',
-                'required': False,
-                'default': 0
-            }
-        },
-        config_file={'param_1': 1}) == {
-            'param_1': 1,
-            'param_2': 0
+def test_generate_component_configuration_settings_non_existing_optional_parameter_w_default():
+    assert component.generate_component_configuration(settings={
+        'param_2': {
+            'description': 'Param 2 description',
+            'required': False,
+            'default': 0
         }
+    },
+                                                      config_file={'param_1': 1}) == {
+                                                          'param_1': 1,
+                                                          'param_2': 0
+                                                      }
