c500989808c9f3da43f99e47def32c666d242e79
Ismael <barracuda.framework@gmail.com>
2020-05-12 12:40:40 +0200

    refactor test misc as class

--- tests/unit_tests/test_utils/test_misc.py
+++ tests/unit_tests/test_utils/test_misc.py
@@ -9,63 +9,73 @@ from mamba_server.components.gui.load_screen.interface import LoadScreenInterfac
 from mamba_server.exceptions import LaunchFileException
 
 
-def test_get_classes_from_module_commands():
-    cmds = misc.get_classes_from_module('mamba_server.commands', MambaCommand)
-    assert len(cmds) == 2
-    assert 'serve' in cmds
-    assert 'startproject' in cmds
+class TestClass:
+    def setup_class(self):
+        """ setup_class called once for the class """
+        pass
 
+    def teardown_class(self):
+        """ teardown_class called once for the class """
+        pass
 
-def test_get_classes_from_module_commands_class_gui_plugin():
-    assert misc.get_classes_from_module('mamba_server.commands',
-                                        GuiPluginInterface) == {}
+    def setup_method(self):
+        """ setup_method called for every method """
+        pass
 
+    def teardown_method(self):
+        """ teardown_method called for every method """
+        pass
 
-def test_get_classes_from_module_components_class_gui_plugin_recursive():
-    classes_dict = misc.get_classes_from_module('mamba_server.components.gui',
-                                                GuiPluginInterface)
-    assert len(classes_dict) == 3
-    assert 'about_qt' in classes_dict
-    assert 'about_tk' in classes_dict
-    assert 'quit' in classes_dict
+    def test_get_classes_from_module_commands(self):
+        cmds = misc.get_classes_from_module('mamba_server.commands', MambaCommand)
+        assert len(cmds) == 2
+        assert 'serve' in cmds
+        assert 'startproject' in cmds
 
+    def test_get_classes_from_module_commands_class_gui_plugin(self):
+        assert misc.get_classes_from_module('mamba_server.commands',
+                                            GuiPluginInterface) == {}
 
-def test_get_classes_from_module_components_class_gui_plugin_subfolder():
-    classes_dict = misc.get_classes_from_module(
-        'mamba_server.components.gui.plugins.about.about_qt',
-        GuiPluginInterface)
-    assert len(classes_dict) == 1
-    assert 'about_qt' in classes_dict
+    def test_get_classes_from_module_components_class_gui_plugin_recursive(self):
+        classes_dict = misc.get_classes_from_module('mamba_server.components.gui',
+                                                    GuiPluginInterface)
+        assert len(classes_dict) == 3
+        assert 'about_qt' in classes_dict
+        assert 'about_tk' in classes_dict
+        assert 'quit' in classes_dict
 
+    def test_get_classes_from_module_components_class_gui_plugin_subfolder(self):
+        classes_dict = misc.get_classes_from_module(
+            'mamba_server.components.gui.plugins.about.about_qt',
+            GuiPluginInterface)
+        assert len(classes_dict) == 1
+        assert 'about_qt' in classes_dict
 
-def test_get_component_valid_id_and_type():
-    components_dict = misc.get_component(
-        'about_qt', 'mamba_server.components.gui.plugins', GuiPluginInterface,
-        Context())
-    assert components_dict is not None
+    def test_get_component_valid_id_and_type(self):
+        components_dict = misc.get_component(
+            'about_qt', 'mamba_server.components.gui.plugins', GuiPluginInterface,
+            Context())
+        assert components_dict is not None
 
+    def test_get_component_valid_id_wrong_type(self):
+        with pytest.raises(LaunchFileException) as excinfo:
+            misc.get_component('about_qt', 'mamba_server.components.gui.plugins',
+                               LoadScreenInterface, Context())
 
-def test_get_component_valid_id_wrong_type():
-    with pytest.raises(LaunchFileException) as excinfo:
-        misc.get_component('about_qt', 'mamba_server.components.gui.plugins',
-                           LoadScreenInterface, Context())
+        assert 'not a valid component identifier' in str(excinfo.value)
 
-    assert 'not a valid component identifier' in str(excinfo.value)
+    def test_get_components_valid_id_and_type(self):
+        components_dict = misc.get_components(
+            ['about_qt', 'quit'], 'mamba_server.components.gui.plugins',
+            GuiPluginInterface, Context())
+        assert len(components_dict) == 2
+        assert 'about_qt' in components_dict
+        assert 'quit' in components_dict
 
+    def test_get_components_invalid_id(self):
+        with pytest.raises(LaunchFileException) as excinfo:
+            misc.get_components(['about_qt', 'about_tk_fail'],
+                                'mamba_server.components.gui.plugins',
+                                GuiPluginInterface, Context())
 
-def test_get_components_valid_id_and_type():
-    components_dict = misc.get_components(
-        ['about_qt', 'quit'], 'mamba_server.components.gui.plugins',
-        GuiPluginInterface, None)
-    assert len(components_dict) == 2
-    assert 'about_qt' in components_dict
-    assert 'quit' in components_dict
-
-
-def test_get_components_invalid_id():
-    with pytest.raises(LaunchFileException) as excinfo:
-        misc.get_components(['about_qt', 'about_tk_fail'],
-                            'mamba_server.components.gui.plugins',
-                            GuiPluginInterface, None)
-
-    assert 'not a valid component identifier' in str(excinfo.value)
+        assert 'not a valid component identifier' in str(excinfo.value)
