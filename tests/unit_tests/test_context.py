25d39265ee5d36dba5caca74163e636ae577561a
Ismael <barracuda.framework@gmail.com>
2020-05-12 12:58:56 +0200

    added test for context memory

--- tests/unit_tests/test_context.py
+++ tests/unit_tests/test_context.py
@@ -14,3 +14,13 @@ def test_context():
     # Update existing parameter
     context.set('param_1', [0, 1, 2])
     assert context.get('param_1') == [0, 1, 2]
+
+
+def test_context_memory_not_static():
+    context_1 = Context()
+
+    context_1.set('param_1', 1)
+    assert context_1.get('param_1') is 1
+
+    context_2 = Context()
+    assert context_2.get('param_1') is None
