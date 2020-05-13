497089bdc45f7ef3de7aad4528404449014b081a
Ismael <barracuda.framework@gmail.com>
2020-05-13 11:36:42 +0200

    typo

--- tests/unit_tests/commands/test_startproject_command.py
+++ tests/unit_tests/commands/test_startproject_command.py
@@ -11,14 +11,14 @@ from importlib import import_module
 
 def get_pythonpath():
     """Return a PYTHONPATH suitable to use in processes so that they find this
-    installation of Scrapy"""
+    installation of Mamba"""
     scrapy_path = import_module('mamba_server').__path__[0]
     return dirname(scrapy_path) + os.pathsep + os.environ.get('PYTHONPATH', '')
 
 
 def get_testenv():
     """Return a OS environment dict suitable to fork processes that need to import
-    this installation of Scrapy, instead of a system installed one.
+    this installation of Mamba, instead of a system installed one.
     """
     env = os.environ.copy()
     env['PYTHONPATH'] = get_pythonpath()
