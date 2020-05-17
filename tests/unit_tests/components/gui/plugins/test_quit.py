9276cacd63888f1edb4fe25ba7e5004ded255cbf
Ismael <barracuda.framework@gmail.com>
2020-05-17 16:56:58 +0200

    added Rx communication test to Quit

--- tests/unit_tests/components/gui/plugins/test_quit.py
+++ tests/unit_tests/components/gui/plugins/test_quit.py
@@ -1,10 +1,11 @@
-from mamba_server.context import Context
+from mamba_server.context_mamba import Context as Context_Mamba
+from mamba_server.context_rx import Context as Context_Rx
 from mamba_server.components.gui.plugins.quit import GuiPlugin
 from mamba_server.components.gui.main_window.main_qt import MainWindow
 
 
 def test_about_gui_plugin_w_menu_window():
-    context = Context()
+    context = Context_Mamba()
     main_window = MainWindow(context)
 
     # Test help is not in menu bar
@@ -33,3 +34,92 @@ def test_about_gui_plugin_w_menu_window():
 
     # Test window is hidden per default
     assert not main_window._app.isVisible()
+
+
+class DummyTestClass:
+    def __init__(self):
+        super(DummyTestClass, self).__init__()
+        self.times_called = 0
+        self.last_value = None
+
+    def test_function(self, rx_on_set=None):
+        self.times_called += 1
+        self.last_value = rx_on_set
+
+
+def test_about_gui_plugin_run_rx_mamba():
+    context = Context_Mamba()
+    dummy_test_class = DummyTestClass()
+    main_window = MainWindow(context)
+
+    context.set('main_window', main_window)
+    widget = GuiPlugin(context)
+
+    # Subscribe to the 'quit' that shall be published
+    context.rx.subscribe('quit', dummy_test_class.test_function)
+
+    # Check it is not activated by another menu
+    context.rx.on_next('run_plugin', {
+        'menu': 'File_Wrong',
+        'action': 'Quit'
+    })
+
+    assert dummy_test_class.times_called == 0
+    assert dummy_test_class.last_value is None
+
+    # Check it is not activated by another action
+    context.rx.on_next('run_plugin', {
+        'menu': 'File',
+        'action': 'Quit_wrong'
+    })
+
+    assert dummy_test_class.times_called == 0
+    assert dummy_test_class.last_value is None
+
+    # Check activation emits 'quit'
+    context.rx.on_next('run_plugin', {
+        'menu': 'File',
+        'action': 'Quit'
+    })
+
+    assert dummy_test_class.times_called == 1
+    assert dummy_test_class.last_value is None
+
+
+def test_about_gui_plugin_run_rx_py():
+    context = Context_Rx()
+    dummy_test_class = DummyTestClass()
+    main_window = MainWindow(context)
+
+    context.set('main_window', main_window)
+    widget = GuiPlugin(context)
+
+    # Subscribe to the 'quit' that shall be published
+    context.rx.subscribe('quit', dummy_test_class.test_function)
+
+    # Check it is not activated by another menu
+    context.rx.on_next('run_plugin', {
+        'menu': 'File_Wrong',
+        'action': 'Quit'
+    })
+
+    assert dummy_test_class.times_called == 0
+    assert dummy_test_class.last_value is None
+
+    # Check it is not activated by another action
+    context.rx.on_next('run_plugin', {
+        'menu': 'File',
+        'action': 'Quit_wrong'
+    })
+
+    assert dummy_test_class.times_called == 0
+    assert dummy_test_class.last_value is None
+
+    # Check activation emits 'quit'
+    context.rx.on_next('run_plugin', {
+        'menu': 'File',
+        'action': 'Quit'
+    })
+
+    assert dummy_test_class.times_called == 1
+    assert dummy_test_class.last_value is None
