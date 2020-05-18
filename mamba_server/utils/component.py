a381557ca7d1a357a42734d2aab23e9e8cc4c87f
Ismael <barracuda.framework@gmail.com>
2020-05-18 16:49:53 +0200

    added merge dict utility

---
+++ mamba_server/utils/component.py
@@ -0,0 +1,13 @@
+""" Mamba generic utility functions for components """
+
+
+def merge_dicts(dict_1, dict_2):
+    """ Merge dictionary dict_2 into dict_1. In case of conflict dict_1 has precedence """
+    result = dict_1
+    for key in dict_2:
+        if key in dict_1:
+            if isinstance(dict_1[key], dict) and isinstance(dict_2[key], dict):
+                merge_dicts(dict_1[key], dict_2[key])
+        else:
+            result[key] = dict_2[key]
+    return result
---
+++ tests/unit_tests/test_utils/test_component.py
@@ -0,0 +1,9 @@
+from mamba_server.utils.component import merge_dicts
+
+
+class TestClass:
+    def test_merge_dicts(self):
+        # Test behaviour with no conflict
+        assert merge_dicts({1: {"a": "A"}, 2: {"b": "B"}}, {2: {"c": "C"}, 3: {"d": "D"}})
+        # Test behaviour with conflict
+        assert merge_dicts({1: {"a": "A"}, 2: {"b": "B"}}, {1: {"a": "A"}, 2: {"b": "C"}})
