57ef1f1e24ca52434510bfac9c5f12f4fd735f7f
Ismael <barracuda.framework@gmail.com>
2020-05-23 10:45:50 +0200

    added keysight m8131a mini controller

---
+++ mamba_server/components/io_controller/digitizer/__init__.py
@@ -0,0 +1,21 @@
+""" Digitizer IO Controller"""
+
+import os
+
+from mamba_server.components.observable_types import IoServiceRequest, Telemetry
+from mamba_server.components.io_controller import VisaControllerBase
+
+
+class Digitizer(VisaControllerBase):
+    """ Digitizer IO Controller class """
+    def __init__(self, context, local_config=None):
+        super(Digitizer, self).__init__(os.path.dirname(__file__),
+                                                context, local_config)
+
+        # Initialize custom variables
+        self._custom_process = [
+        ]  # All the services that require custom processing
+
+    def _service_preprocessing(self, service_request: IoServiceRequest, result: Telemetry):
+        """ Entry point for running a component service """
+        pass
