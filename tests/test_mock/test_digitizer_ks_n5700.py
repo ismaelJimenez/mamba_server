7754304a26cda16de63766b635694509252116e2
Ismael <barracuda.framework@gmail.com>
2020-05-29 20:20:31 +0200

    added mock for n5700 power supply

---
+++ tests/test_mock/test_digitizer_ks_n5700.py
@@ -0,0 +1,51 @@
+import os
+import pyvisa
+
+MOCK_FILE = os.path.join('mock', 'visa', 'power_supply', 'ks_n5700.yaml')
+INST_ADDRESS = 'TCPIP0::1.2.3.4::INSTR'
+
+
+class TestClass:
+    def setup_class(self):
+        """ setup_class called once for the class """
+        pass
+
+    def teardown_class(self):
+        """ teardown_class called once for the class """
+        pass
+
+    def setup_method(self):
+        """ setup_method called for every method """
+        mamba_dir = os.path.join(os.path.dirname(__file__), '..', '..',
+                                 'mamba')
+        asd = os.path.join(mamba_dir, MOCK_FILE)
+        self.visa_inst = pyvisa.ResourceManager(
+            f"{os.path.join(mamba_dir, MOCK_FILE)}@sim").open_resource(
+                INST_ADDRESS, read_termination='\n')
+
+    def teardown_method(self):
+        """ teardown_method called for every method """
+        self.visa_inst.close()
+
+    def test_dialogues(self):
+        assert self.visa_inst.query(
+            '*IDN?') == 'Keysight Technologies, N5700, SN XXXX, FW YYYY'
+
+    def test_properties(self):
+        self.visa_inst.write('*RST')
+
+        assert self.visa_inst.query('CURR?') == '0'
+        self.visa_inst.write('CURR 1')
+        assert self.visa_inst.query('CURR?') == '1'
+
+        assert self.visa_inst.query('VOLT?') == '0'
+        self.visa_inst.write('VOLT 5')
+        assert self.visa_inst.query('VOLT?') == '5'
+
+        assert self.visa_inst.query('VOLT:PROT:LEV?') == '0'
+        self.visa_inst.write('VOLT:PROT:LEV 10')
+        assert self.visa_inst.query('VOLT:PROT:LEV?') == '10'
+
+        assert self.visa_inst.query('OUTP:STAT?') == '0'
+        self.visa_inst.write('OUTP:STAT 1')
+        assert self.visa_inst.query('OUTP:STAT?') == '1'
