d6f3dfbb97655475146f3bc5c803e8a7547e540d
Meti <eti>
2020-06-02 13:16:12 +0200

    added 5747 and 33502 controllers

---
+++ mamba/component/io_controller/power_supply/ks_n5747a/__init__.py
@@ -0,0 +1,14 @@
+""" Digitizer IO Controller"""
+
+import os
+
+from mamba.core.msg import ServiceRequest,\
+    ServiceResponse
+from mamba.component.io_controller import VisaControllerBase
+
+
+class PowerSupply(VisaControllerBase):
+    """ Digitizer IO Controller class """
+    def __init__(self, context, local_config=None):
+        super(PowerSupply, self).__init__(os.path.dirname(__file__), context,
+                                        local_config)
