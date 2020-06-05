f88ff5bd6fb8cf5872e6ba70a4afc0c2bfeb5e4a
Ismael <barracuda.framework@gmail.com>
2020-06-05 20:59:32 +0200

    refactor about qt

--- mamba/component/plugins/about/about_qt/__init__.py
+++ mamba/component/plugins/about/about_qt/__init__.py
@@ -4,6 +4,7 @@ import os
 import pkgutil
 
 from PySide2.QtWidgets import QMessageBox, QWidget, QApplication
+from PySide2.QtGui import QIcon
 
 from mamba.component.plugins import PluginBase
 from mamba.component.gui.msg import RunAction
@@ -31,6 +32,10 @@ class Plugin(PluginBase):
         self._version = pkgutil.get_data('mamba',
                                          'VERSION').decode('ascii').strip()
         self._box_message = f"Mamba Server v{self._version}"
+        self._app.setWindowIcon(
+            QIcon(
+                os.path.join(self._context.get('mamba_dir'), 'artwork',
+                             'mamba_icon.png')))
 
     def run(self, rx_value: RunAction):
         """ Entry point for running the plugin
@@ -38,5 +43,16 @@ class Plugin(PluginBase):
             Args:
                 rx_value (RunAction): The value published by the subject.
         """
-        QMessageBox.about(QWidget(), self._configuration['message_box_title'],
+        def center(widget):
+            frame_gm = widget.frameGeometry()
+            screen = QApplication.desktop().screenNumber(
+                QApplication.desktop().cursor().pos())
+            center_point = QApplication.desktop().screenGeometry(
+                screen).center()
+            frame_gm.moveCenter(center_point)
+            widget.move(frame_gm.topLeft())
+
+        widget = QWidget()
+        center(widget)
+        QMessageBox.about(widget, self._configuration['message_box_title'],
                           self._box_message)
