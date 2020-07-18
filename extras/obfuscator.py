0122d295ef50c1d9d3b0c863acdefa0a18efe6f3
Ismael <barracuda.framework@gmail.com>
2020-07-18 23:58:22 +0200

    improved obfuscator

--- extras/obfuscator.py
+++ extras/obfuscator.py
@@ -11,7 +11,7 @@ def get_random_alphanumeric_string():
     return random.choice(string.ascii_letters) + ''.join((random.choice(letters_and_digits) for i in range(length)))
 
 
-rootdir = ['mamba', 'tests']
+rootdir = ['mamba/core', 'tests']
 
 blank_mapping = [
     ' -> None',
