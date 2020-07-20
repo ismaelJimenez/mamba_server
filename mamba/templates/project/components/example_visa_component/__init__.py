1e0f3b8d5526aab1ae7d62bd58c6a3ed985abbab
Ismael <barracuda.framework@gmail.com>
2020-07-20 12:29:47 +0200

    added mock licenses

--- mamba/templates/project/components/example_visa_component/__init__.py
+++ mamba/templates/project/components/example_visa_component/__init__.py
@@ -1,4 +1,44 @@
-""" Keysight Series N5700 DC Power Supply Controller"""
+#############################################################################
+##
+## Copyright (C) 2020 The Mamba Company.
+## Contact: themambacompany@gmail.com
+##
+## This file is part of Mamba Server.
+##
+## $MAMBA_BEGIN_LICENSE:LGPL$
+## Commercial License Usage
+## Licensees holding valid commercial Mamba licenses may use this file in
+## accordance with the commercial license agreement provided with the
+## Software or, alternatively, in accordance with the terms contained in
+## a written agreement between you and The Mamba Company. For licensing terms
+## and conditions see LICENSE. For further information contact us at
+## themambacompany@gmail.com.
+##
+## GNU Lesser General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU Lesser
+## General Public License version 3 as published by the Free Software
+## Foundation and appearing in the file LICENSE.LGPL3 included in the
+## packaging of this file. Please review the following information to
+## ensure the GNU Lesser General Public License version 3 requirements
+## will be met: https://www.gnu.org/licenses/lgpl-3.0.html.
+##
+## GNU General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU
+## General Public License version 2.0 or (at your option) the GNU General
+## Public license version 3 or any later version approved by the KDE Free
+## Qt Foundation. The licenses are as published by the Free Software
+## Foundation and appearing in the file LICENSE.GPL2 and LICENSE.GPL3
+## included in the packaging of this file. Please review the following
+## information to ensure the GNU General Public License requirements will
+## be met: https://www.gnu.org/licenses/gpl-2.0.html and
+## https://www.gnu.org/licenses/gpl-3.0.html.
+##
+## $MAMBA_END_LICENSE$
+##
+#############################################################################
+
+""" Example VISA Controller"""
+
 from typing import Optional
 import os
 
@@ -6,8 +46,8 @@ from mamba.core.context import Context
 from mamba.core.component_base import VisaInstrumentDriver
 
 
-class PowerSupplyKsN5700(VisaInstrumentDriver):
-    """ Keysight Series N5700 DC Power Supply Controller class """
+class ExampleController(VisaInstrumentDriver):
+    """ Example VISA Controller class """
     def __init__(self,
                  context: Context,
                  local_config: Optional[dict] = None) -> None:
